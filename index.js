f5c0a7c03793ea5845f52b2fa85d6b4f91d6c3b2
Robert Bubnik <rbubnik@gmail.com>
2023-09-18 19:09:55 -0400

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your Circle class here
+
+class Circle {
+    
+    constructor (radius) {
+        this.radius = radius
+    }
+    get diameter() {
+        return this.radius * 2
+    }
+
+    get circumference () {
+        let pi = Math.PI
+        return (this.radius *2) * pi
+    }
+
+    get area() {
+        let pi = Math.PI
+        return pi * (this.radius * this.radius)
+    }
+
+    set diameter(newDiameter) {
+        this.radius = newDiameter/2
+    }
+
+    set circumference( newCircum) {
+        let pi = Math.PI
+        this.radius = (newCircum/2) / pi
+    }
+
+    set area(newArea) {
+        let pi = Math.PI
+        this.radius = Math.sqrt(newArea/pi)
+    }
+}
+
